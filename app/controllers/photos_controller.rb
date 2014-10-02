e49ba8fa88307ed6f7d47f01aadb874821e3256c
Pedro Taglialatela <pedro@pordotom.com.br>
2014-10-02 11:30:47 -0300

    Criando form para cadastrar nova imagem

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -12,4 +12,23 @@ class PhotosController < ApplicationController
 		end
 	end
 
+	def new
+		@photo = Photo.new
+	end
+
+	def create
+		@photo = Photo.new(photo_params)
+		if @photo.save
+			flash[:notice] = "Imagem criada com sucesso."
+			redirect_to photos_path
+		else
+			render :action => "new"
+		end
+	end
+
+	private
+		def photo_params   
+			params.require(:photo).permit(:title, :url, :photo)
+		end
+
 end
