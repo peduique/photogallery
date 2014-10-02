31013f7a6db0191784c648c4f0156e7b2b678a40
Pedro Taglialatela <pedro@pordotom.com.br>
2014-10-02 11:35:46 -0300

    Adicionando config paperclip no modelo

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,20 @@
 class Photo < ActiveRecord::Base
+
+	validates_presence_of :nome
+
+	has_attached_file :photo,
+		:path => ":rails_root/public/system/:class/:attachement/:id/:basename_:style.:extension",
+		:url => "/system/:class/:attachement/:id/:basename_:style.:extension",
+		:styles => { 
+			:banner => "640x480#",
+			:thumb => "100x100>"
+		},
+		:default_url => "/images/:style/missing.png"
+
+
+	validates_attachment :photo,
+		:presence => true,
+		:size => { :in => 0..10.megabytes },
+		:content_type => { :content_type => /^image\/(jpeg|png|gif|tiff)$/ }
+
 end
