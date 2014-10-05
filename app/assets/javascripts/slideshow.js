007b2ec3d84c48a75ec479e22ca7539d01b83588
Pedro Taglialatela <pedro@pordotom.com.br>
2014-10-05 12:36:49 -0300

    criacao script slide

---
+++ app/assets/javascripts/slideshow.js
@@ -0,0 +1,53 @@
+var Slider = (function() {
+
+	var current,
+		slides,
+		images;
+
+	return {
+		initialize: function() {
+			slider = document.getElementsByClassName('slider')[0];
+			images = slider.querySelectorAll("figure");
+			slides = images.length;
+
+			current = -1; 
+			Slider.navigation();
+			Slider.play();
+		},
+
+		navigation: function() {
+			navigation = document.getElementsByClassName('slideshow-nav')[0];
+			var prevSlide = navigation.querySelector('.prev-slide'),
+				nextSlide = navigation.querySelector('.next-slide');
+
+			prevSlide.addEventListener('click', function() {
+				if (current > 0) { 
+					current -= 2 
+					Slider.play();
+				} 
+			}, false);
+
+			nextSlide.addEventListener('click', function() {
+				if ( ( current + 1 ) < slides ) {
+					Slider.play();
+				}
+			}, false);
+		},
+
+		play: function() {
+			var position = document.getElementsByClassName('slider-position')[0];
+			position.innerHTML = ( current + 2 ) + ' de ' + slides;
+
+			current = ( current + 1 ) % slides;
+
+			for (var i = 0; i < slides; i++) {
+				if ( i === current ) {
+					images[i].classList.remove("hide");
+				} else {
+					images[i].classList.add("hide");
+				}
+			}
+
+		}
+	};
+})();
